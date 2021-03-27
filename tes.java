d912edf8a68944dd72e8d64193752da89423115f
Ekram007 <ei.ekramul.islam@gmail.com>
2021-03-27 17:55:43 +0600

    Project inishialized

--- tes.java
+++
@@ -1,9 +0,0 @@
-public class tes {
-    public static void main(String[] args) {
-        //System.out.format("%32s%10d%16s", "Hello", 20, "There");
-        String name = "jsd";
-        Double d = 5.25;
-
-        System.out.format(name,d);
-    }
-}
